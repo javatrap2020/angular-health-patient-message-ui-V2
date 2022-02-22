50024ead69323e3807eb68c811d371d77706af1d
Oleh Boiko <javatrap2020@gmail.com>
2022-02-22 18:01:36 +0200

    fix pipeline 008

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,7 @@ sudo service docker start
 sudo service docker start
 cd /home/ec2-user/server
 sudo rm -rf .dockerignore
+sudo rm -rf Dockerfile
 sudo rm -rf .gitignor
 cd /home/ec2-user/server/.github/workflows/
 sudo rm -rf docker-image.yml
@@ -12,3 +13,4 @@ cd /home/ec2-user/server
 sudo docker-compose up
 sudo rm -rf .dockerignore
 sudo rm -rf .gitignore
+sudo rm -rf Dockerfile
